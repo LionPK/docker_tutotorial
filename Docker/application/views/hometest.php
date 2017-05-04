683b1da1360052285bd139ba62d63fecc02decc1
LionPK <praneed.kla@northbkk.ac.th>
2017-05-04 10:44:27 +0700

    v1
    docker tutorial version 1

---
+++ Docker/application/views/hometest.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgnitre</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("resources/css/bootsrap.min.css"); ?>"/>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				
+				<a class="navbar-brand" href="#">
+					CodeIgniter
+				</a>
+			</div>
+		</div>
+	</nav>
+	<div id="container">
+		<h1>Welcome to CodeIgniter!</h1>
+	</div>
+	<script type="text/js" src="<?php echo base_url("resources/js/bootstrap.js"); ?>"></script>
+	<script type="text/js" src="<?php echo base_url("resources/js/jquery-3.1.0.js"); ?>"></script>
+</body>
